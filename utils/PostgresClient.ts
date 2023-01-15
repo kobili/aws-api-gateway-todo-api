15ce61178acc6f2cbb80c60ebbd242da0ce72b28
Kobe Li <likobe23@gmail.com>
2023-01-15 02:40:40 -0800

    Add .env file

--- utils/PostgresClient.ts
+++ utils/PostgresClient.ts
@@ -1,9 +1,11 @@
 import { Client } from 'ts-postgres';
 
+require('dotenv').config();
+
 export const pgClient = new Client({
-    host: '<DATABASE_URL>',
-    port: <DATABASE_PORT>,
-    database: '<DATABASE_NAME>',
-    user: '<DATABASE_USERNAME>',
-    password: '<DATABASE_PASSWORD>'
+    host: process.env.PG_URL,
+    port: parseInt(process.env.PG_PORT!),
+    database: process.env.PG_DATABASE,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD
 });
