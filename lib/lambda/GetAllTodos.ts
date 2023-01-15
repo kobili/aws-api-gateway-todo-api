b5fd23f0ac1b00a8734050b7027bc21e8f42d6f3
Kobe Li <likobe23@gmail.com>
2023-01-15 14:48:21 -0800

    Update GetAllTodos

--- lib/lambda/GetAllTodos.ts
+++ lib/lambda/GetAllTodos.ts
@@ -1,5 +1,6 @@
 import { APIGatewayEvent, APIGatewayProxyHandler, Context } from "aws-lambda";
 import { pgClient } from "../../utils/PostgresClient";
+import { Client } from "ts-postgres";
 
 /**
  * handles logic for the endpoint: GET /todo/all
@@ -9,18 +10,27 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayEvent, co
 
     await pgClient.connect();
 
-    const dbQueryResult = pgClient.query(
-        "SELECT * FROM todo"
-    );
-
-    let data = [];
-
-    for await (const result of dbQueryResult) {
-        data.push(result);
+    try {
+        const dbQueryResult = pgClient.query(
+            "SELECT * FROM todo"
+        );
+    
+        let data = [];
+    
+        for await (const result of dbQueryResult) {
+            data.push(result);
+        }
+    
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data)
+        };
+    } catch (e: unknown) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify(e)
+        }
+    } finally {
+        await pgClient.end();
     }
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify(data)
-    };
 };
